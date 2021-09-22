12d19fb4940e8be267bf684987cd01619535a377
santhosh03a <69502837+santhosh03a@users.noreply.github.com>
2021-09-22 11:16:01 +0530

    Add files via upload

---
+++ day1.sql
@@ -0,0 +1,22 @@
+use db1;
+-- creating a table
+create table student(
+id int primary key,
+name varchar(20),
+marks varchar(20),
+dept_id int
+);
+create table student(
+dept_id int primary key,
+D_name varchar(20)
+);
+
+-- entering the data into a table
+insert into department values(1,'CSE'),(2,'ISE'),(3,'ECE');
+insert into department values(4,'EEE'),(5,'AE'),(6,'ME');
+insert into student values(4,'raju',600,4),(5,'John',350,4),(6,'Kevin',550,6);
+insert into student values(7,'raju',600,5);
+
+-- select command
+select * from student;
+select * from department;
---
+++ day2.sql
@@ -0,0 +1,80 @@
+-- where clause
+ 
+select * from student s,department d where s.D_id=d.D_id;
+
+select * from student where d_id>1;
+
+
+
+select * from department where Department_name='ise';
+
+
+-- subqueries
+
+select * from student where D_id=(select D_id from department where Department_name='cse');
+
+
+-- in keyword is used when we have to check the data for multiple rows.
+
+select * from student where D_id in (select D_id from department where Department_name='ise'||department_name='cse');
+
+
+-- update command
+
+UPDATE student SET marks=500 WHERE name='santhosh';
+
+
+-- delete command
+
+delete from department where id=3;
+
+
+-- alter command - used to add or remove columns in a table
+
+alter table student add result varchar(20);
+
+alter table student drop result;
+
+
+create table sales(
+
+sales_id int primary key,
+
+salesname varchar(20),
+
+address varchar(20)
+);
+
+
+-- applying foreign key and primary key
+
+create table order_sales(
+
+id int primary key,
+
+product_name varchar(20),
+
+address varchar(30),
+
+sales_id int,
+
+constraint fk_sales foreign key(sales_id)
+references sales(sales_id)
+);
+
+
+select * from order_sales;
+
+select * from sales;
+
+insert into sales values(1,'suresh','Bangalore'),(2,'ramesh','mysore'),(3,'rohit','mumbai');
+
+insert into order_sales values(1,'a','Bangalore',3),(2,'b','bangalore',3),(3,'c','mumbai',1);
+
+
+-- join clauses
+
+select * from sales s inner join order_sales p on s.address=p.address;
+
+select * from sales s left join order_sales p on s.address=p.address;
+select * from sales s right join order_sales p on s.address=p.address;
